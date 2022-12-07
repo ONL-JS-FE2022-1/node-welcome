66f38bd7c4ea72081695992c254de22615564e28
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-12-07 18:49:24 +0200

    Npm init

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 'use strict';
 
-const fs = require('fs').promises;
-
-fs.readFile('./text.txt', 'utf-8')
-.then(content => {
-    const newContent = `OLD content: ${content} and NEW content: 'Again hello'`;
-    // fs.writeFile('./newfile.txt', newContent, 'utf8'); // буде перезаписувати файл
-    fs.appendFile('./newfile.txt', 'AND APPEND', 'utf8'); // буде додавати в кінець
-});
+console.log('123');
